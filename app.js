f7423d13878c284bcb126a7f14c1b103d8e3951c
uzer <uzer@Ubuntu.myguest.virtualbox.org>
2024-02-19 13:42:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static('./public'));
+
+app.get('/api/v1/', (req, res) => {
+    res.status(200).json({item: 'requrest', status: 'success'});
+})
+
+app.listen(port, () => {console.log(`The server is listening on port ${port}`)});
